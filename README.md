39b364302c204612e05664879a8c02d3b24a50a7
unknown <sjjoseph106@gmail.com>
2022-09-10 17:47:15 +0900

    JSDoc 리팩토링

--- README.md
+++ README.md
@@ -3,13 +3,9 @@
 ### 👀 Todo 앱을 JSDoc으로 문서화해보자
 
 - todoDataList
-  - 할 일 목록의 데이터 객체를 담는 배열입니다
-  - 태그들은 `Optional property`로 처리했습니다
-  - `typedef`로 todoData를 따로 선언하고 todoDataList 배열에 넣어줬습니다
 
 ```
 /**
- * 필요한 데이터를 모두 모델링한다.
  * @typedef todoData
  * @type {Object}
  * @property {string} id - 아이디
@@ -19,33 +15,31 @@
  * @property {string[]} [tags] - 태그들
  */
 
-/** @type {Array<todoData>} */
+/**
+ * 할 일 목록의 데이터 객체를 담는 배열입니다.
+ * @type {Array<todoData>}
+ */
 ```
 
 - createTodo
-  - 할 일을 추가하는 함수입니다
-  - 태그들은 `Optional parameter`로 처리했습니다
+  - 할 일을 추가할 수 있다
+  - 내용없이 추가할 수 없다
 
 ```
-/**
- * 할 일을 추가할 수 있다.
- * 내용 없이 추가할 수 없다.
+ * 할 일을 추가하는 함수입니다.
  * @param {string} content - 내용
  * @param {string} category - 카테고리
  * @param {string[]} [tags] - 태그들
- */
 
 ```
 
 - readTodo
-  - 할 일을 조회하는 함수입니다
-  - 아이디는 `Optional parameter`로 처리했습니다
-  - 아이디가 존재한다면 특정 할 일을 반환하고, 없다면 모든 할 일을 반환합니다
+  - 모든 할 일을 조회할 수 있다
+  - ID를 기반으로 특정 할 일을 조회할 수 있다
 
 ```
 /**
- * 모든 할 일을 조회할 수 있다.
- * ID를 기반으로 특정 할 일을 조회할 수 있다.
+ * 할 일을 조회하는 함수입니다.
  * @param {string} [id] - 아이디
  * @returns {Array<todoData> | todoData} - 데이터
  */
@@ -53,13 +47,12 @@
 ```
 
 - updateTodo
-  - 할 일을 수정하는 함수입니다
-  - 특정 태그도 수정하는 경우 태그 위치를 `Optional parameter`로 가집니다
+  - ID를 제외한 모든 속성을 수정할 수 있다
+  - 특정 할 일의 특정 태그를 수정할 수 있다
 
 ```
 /**
- * ID를 제외한 모든 속성을 수정할 수 있다.
- * 특정 할 일의 특정 태그를 수정할 수 있다.
+ * 할 일을 수정하는 함수입니다.
  * @param {string} id - 아이디
  * @param {number} [tagIndex] - 태그 위치
  */
@@ -67,16 +60,14 @@
 ```
 
 - deleteTodo
-  - 할 일을 삭제하는 함수입니다
-  - 특정 할 일의 모든 태그를 삭제하는 경우 아이디만 `Optional parameter`로 가집니다
-  - 특정 할 일의 특정 태그만 삭제하는 경우 태그 위치도 `Optional parameter`로 가집니다
+  - ID를 기반으로 특정 할 일을 삭제할 수 있다
+  - 모든 할 일을 제거할 수 있다
+  - 특정 할 일의 특정 태그를 삭제할 수 있다
+  - 특정 할 일의 모든 태그를 제거할 수 있다
 
 ```
 /**
- * ID를 기반으로 특정 할 일을 삭제할 수 있다.
- * 모든 할 일을 제거할 수 있다.
- * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * 할 일을 삭제하는 함수입니다.
  * @param {string} [id] - 아이디
  * @param {number} [tagIndex] - 태그 위치
  */
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@
  */
 
 /**
- * 필요한 데이터를 모두 모델링한다.
  * @typedef todoData
  * @type {Object}
  * @property {string} id - 아이디
@@ -15,19 +14,20 @@
  * @property {string[]} [tags] - 태그들
  */
 
-/** @type {Array<todoData>} */
+/**
+ * 할 일 목록의 데이터 객체를 담는 배열입니다.
+ * @type {Array<todoData>}
+ */
 const todoDataList = [];
 
 /**
- * 할 일을 추가할 수 있다.
- * 내용 없이 추가할 수 없다.
+ * 할 일을 추가하는 함수입니다.
  * @param {string} content - 내용
  * @param {string} category - 카테고리
  * @param {string[]} [tags] - 태그들
  */
 const createTodo = (content, category, tags) => {
-  if (!content || !category)
-    return console.log("내용과 카테고리를 모두 입력해주세요.");
+  if (!content || !category) throw "내용과 카테고리를 모두 입력해주세요.";
   if (!tags) tags = [];
   todoDataList.push({
     id: `${Date.now()}`,
@@ -39,46 +39,39 @@ const createTodo = (content, category, tags) => {
 };
 
 /**
- * 모든 할 일을 조회할 수 있다.
- * ID를 기반으로 특정 할 일을 조회할 수 있다.
+ * 할 일을 조회하는 함수입니다.
  * @param {string} [id] - 아이디
  * @returns {Array<todoData> | todoData} - 데이터
  */
 const readTodo = (id) => {
-  if (!id) return console.log(todoDataList);
-  return todoDataList[id] === undefined
-    ? console.log("아이디에 해당하는 할 일 데이터가 없습니다.")
-    : console.log(todoDataList[id]);
+  if (!id) return todoDataList;
+  if (todoDataList[id] === undefined)
+    throw "아이디에 해당하는 할 일 데이터가 없습니다.";
+  return todoDataList[id];
 };
 
 /**
- * ID를 제외한 모든 속성을 수정할 수 있다.
- * 특정 할 일의 특정 태그를 수정할 수 있다.
+ * 할 일을 수정하는 함수입니다.
  * @param {string} id - 아이디
  * @param {number} [tagIndex] - 태그 위치
  */
 const updateTodo = (id, tagIndex) => {
-  if (!id) return console.log("아이디를 입력해주세요.");
-  if (tagIndex) return console.log("특정 태그를 수정합니다.");
-  return console.log("할 일 데이터를 수정합니다.");
+  if (!id) throw "아이디를 입력해주세요.";
+  // 할 일 데이터를 수정하는 코드
 };
 
 /**
- * ID를 기반으로 특정 할 일을 삭제할 수 있다.
- * 모든 할 일을 제거할 수 있다.
- * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * 할 일을 삭제하는 함수입니다.
  * @param {string} [id] - 아이디
  * @param {number} [tagIndex] - 태그 위치
  */
 const deleteTodo = (id, tagIndex) => {
-  if (!id && !tagIndex) todoDataList.splice(0);
-  if (!id && tagIndex)
-    return console.log("해당 태그를 삭제할 할 일의 아이디를 입력해주세요.");
-  if (todoDataList[id] === undefined)
-    return console.log("아이디에 해당하는 할 일 데이터가 없습니다.");
-  const targetIndex = todoDataList.findIndex((todo) => todo.id === id);
-  tagIndex
-    ? todoDataList[targetIndex].tags.splice(tagIndex, 1)
-    : todoDataList[targetIndex].tags.splice(0);
+  if (id) {
+    if (todoDataList[id] === undefined)
+      throw "아이디에 해당하는 할 일 데이터가 없습니다.";
+    const targetIndex = todoDataList.findIndex((todo) => todo.id === id);
+    tagIndex
+      ? todoDataList[targetIndex].tags.splice(tagIndex, 1)
+      : todoDataList[targetIndex].tags.splice(0);
+  } else todoDataList.splice(0);
 };
